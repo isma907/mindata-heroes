eed7f1ac50663d2c37deee839068dc9d42504255
Ismael <isma907@gmail.com>
2024-02-12 13:43:04 -0300

    Main App structure

---
+++ src/app/app.routes.ts
@@ -0,0 +1,4 @@
+import { Routes } from '@angular/router';
+import { HeroesListComponent } from './heroes-list/heroes-list.component';
+
+export const routes: Routes = [{ path: '', component: HeroesListComponent }];
---
+++ src/app/heroes-list/heroes-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mindata-heroes-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './heroes-list.component.html',
+  styleUrl: './heroes-list.component.scss'
+})
+export class HeroesListComponent {
+
+}
